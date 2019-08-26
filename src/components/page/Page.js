7db42c0aaef5610d326e079b42eee9857235bfc2
daisu8e <katsumata.coffee.150yen@gmail.com>
2019-08-26 13:15:15 -0700

    add page component

---
+++ src/components/page/Page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Page.css';
+
+function Page() {
+  return (
+    <div className="Page">
+      <header>
+        <h1>Daisuke Katsumata</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Page;
