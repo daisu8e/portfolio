6d0508f5325b7bfa7b32e616740cff0cb048d0f8
daisu8e <katsumata.coffee.150yen@gmail.com>
2019-12-20 10:58:48 -0800

    disabled all

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React, {FC} from 'react';
-import {Switch, Route} from 'react-router-dom';
+//import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
-import items from './items';
-
-import Home from './components/Home';
-import Page1 from './components/Page1';
-import ClassComponent from './ClassComponent';
-import FunctionComponent from './FunctionComponent';
-import ClassComponentProps from './ClassComponentProps';
-import FunctionComponentProps from './FunctionComponentProps';
-import ClassComponentState from './ClassComponentState';
-import FunctionComponentState from './FunctionComponentState';
-import ClassComponentLifecycle from './ClassComponentLifecycle';
-import FunctionComponentLifecycle from './FunctionComponentLifecycle';
-
-import Samples from './Samples';
-new Samples().log();
+//import items from './items';
+//
+//import Home from './components/Home';
+//import Page1 from './components/Page1';
+//import ClassComponent from './ClassComponent';
+//import FunctionComponent from './FunctionComponent';
+//import ClassComponentProps from './ClassComponentProps';
+//import FunctionComponentProps from './FunctionComponentProps';
+//import ClassComponentState from './ClassComponentState';
+//import FunctionComponentState from './FunctionComponentState';
+//import ClassComponentLifecycle from './ClassComponentLifecycle';
+//import FunctionComponentLifecycle from './FunctionComponentLifecycle';
+//
+//import Samples from './Samples';
+//new Samples().log();
 
 const App: FC = () => {
   return (
     <div className="App">
+      {/*
       <Switch>
         <Route path="/page1" component={Page1} />
         <Route component={Home} />
@@ -33,6 +34,7 @@ const App: FC = () => {
       <FunctionComponentState />
       <ClassComponentLifecycle />
       <FunctionComponentLifecycle />
+      */}
     </div>
   );
 };
