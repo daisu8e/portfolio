09c5ed61aafebf1315c1f9a34eebe9559d1dbe4a
daisu8e <katsumata.coffee.150yen@gmail.com>
2020-01-03 15:02:26 -0800

    refactoring

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,25 +4,23 @@ import {configureStore} from './samples/store';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import * as views from './views';
-import './index.css';
+import './index.scss';
 
 export const App: FC = () => (
-  <div className="App">
-    <BrowserRouter>
-      <Provider store={configureStore()}>
-        <Switch>
-          <Route path="/view-a" component={views.ViewA} />
-          <Route path="/view-b" component={views.ViewB} />
-          <Route path="/view-c" component={views.ViewC} />
-          <Route path="/view-d" component={views.ViewD} />
-          <Route path="/view-e" component={views.ViewE} />
-          <Route path="/view-f" component={views.ViewF} />
-          <Route path="/view-g" component={views.ViewG} />
-          <Route path="/view-h" component={views.ViewH} />
-          <Route path="/view-i" component={views.ViewI} />
-          <Route component={views.Home} />
-        </Switch>
-      </Provider>
-    </BrowserRouter>
-  </div>
+  <BrowserRouter>
+    <Provider store={configureStore()}>
+      <Switch>
+        <Route path="/view-a" component={views.ViewA} />
+        <Route path="/view-b" component={views.ViewB} />
+        <Route path="/view-c" component={views.ViewC} />
+        <Route path="/view-d" component={views.ViewD} />
+        <Route path="/view-e" component={views.ViewE} />
+        <Route path="/view-f" component={views.ViewF} />
+        <Route path="/view-g" component={views.ViewG} />
+        <Route path="/view-h" component={views.ViewH} />
+        <Route path="/view-i" component={views.ViewI} />
+        <Route component={views.Home} />
+      </Switch>
+    </Provider>
+  </BrowserRouter>
 );
