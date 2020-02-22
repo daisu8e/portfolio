bf0a1e7e551f7340ef4a43c9ed79b64a47ef3c78
daisu8e <katsumata.coffee.150yen@gmail.com>
2020-02-21 17:31:17 -0800

    returned warnings

--- src/App/views/interfaces/Content/index.tsx
+++ src/App/views/interfaces/Content/index.tsx
@@ -2,13 +2,18 @@ import React, {FC} from 'react';
 
 import {Author} from 'App/models/entities/Author';
 
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Link, Switch, Route, Redirect} from 'react-router-dom';
+import {faEllipsisV} from '@fortawesome/free-solid-svg-icons';
+import {faFilePdf} from '@fortawesome/free-regular-svg-icons';
+import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {MobileNav} from 'App/views/components/MobileNav';
+import {Logo} from 'App/views/components/Logo';
 import {About} from './About';
 import {Work} from './Work';
 import {Theory} from './Theory';
 import {Skills} from './Skills';
-import {Body, Mobile} from './index.styled';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Name, Menu, Sns, Body, Mobile} from './index.styled';
 
 interface Props {
   author: Author;
@@ -19,8 +24,28 @@ export const Content: FC<Props> = props => {
 
   const {author} = props;
 
+  function getHereClass(location: string): string {
+    return props.location.pathname === location ? 'here' : '';
+  }
+
   return (
     <>
+      {/*
+      <Name>
+        <Link to="">{author.name}</Link>
+      </Name>
+      <Menu>
+        <Link to="/about" className={getHereClass('/about')}>About</Link>
+        <Link to="/work" className={getHereClass('/work')}>Work</Link>
+        <Link to="/theory" className={getHereClass('/theory')}>Theory</Link>
+        <Link to="/skills" className={getHereClass('/skills')}>Skills</Link>
+      </Menu>
+      <Sns>
+        <a href="https://github.com/daisu8e/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub}/></a>
+        <a href="https://www.linkedin.com/in/daisu8e/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin}/></a>
+        <a href="/daisuke-katsumata.pdf" target="_blank"><FontAwesomeIcon icon={faFilePdf}/></a>
+      </Sns>
+      */}
       <Body>
         <Switch>
           <Route path="/about" render={p => <About {...p} author={author}/>}/>
